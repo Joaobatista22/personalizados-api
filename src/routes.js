981fde6441f3e3091d9918fddea6625364737999
Joaobatista22 <joaobatiista.ds@gmail.com>
2024-11-10 17:47:08 -0300

    feat: adcionado o multer para upload de img

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import ProductController from "./app/controllers/ProductController";
 
 const router = new Router();
 
+const upload = multer(multerConfig);
+
 router.post("/users", UserController.store);
 router.post("/session", SessionController.store);
+router.post("/products", upload.single("file"), ProductController.store);
 
 export default router;
