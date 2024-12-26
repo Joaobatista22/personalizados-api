e93ac799258f276d2a122a8e436be33ec9c9fd2f
Joaobatista22 <joaobatiista.ds@gmail.com>
2024-12-26 18:37:41 -0300

    backend ok

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -21,7 +21,7 @@ class CategoryController {
 				.status(403)
 				.json({ error: "Access denied: only admins can create categories" });
 		}
-
+		const { filename: path } = req.file;
 		const { name } = req.body;
 
 		const categoryExists = await Category.findOne({
@@ -35,9 +35,70 @@ class CategoryController {
 
 		const { id } = await Category.create({
 			name,
+			path,
 		});
 
 		return res.status(201).json({ id, name });
+	}
+
+	async update(req, res) {
+		const schema = Yup.object({
+			name: Yup.string(),
+		});
+
+		try {
+			schema.validateSync(req.body, { abortEarly: false });
+		} catch (err) {
+			return res.status(400).json({ error: err.errors });
+		}
+
+		const { admin: isAdmin } = (await User.findByPk(req.userId)) || {};
+
+		if (!isAdmin) {
+			return res
+				.status(403)
+				.json({ error: "Access denied: only admins can create categories" });
+		}
+
+		const { id } = req.params;
+
+		const categoryExists = await Category.findByPk(id);
+		if (!categoryExists) {
+			return res
+				.status(400)
+				.json({ message: "Make sure your category ID is correct" });
+		}
+
+		let path;
+		if (req.file) {
+			path = req.file.filename;
+		}
+		const { name } = req.body;
+
+		if (name) {
+			const categoryNameExists = await Category.findOne({
+				where: {
+					name,
+				},
+			});
+			if (categoryNameExists && categoryNameExists.id !== +id) {
+				return res.status(400).json({ error: "Category name already exists" });
+			}
+		}
+
+		await Category.update(
+			{
+				name,
+				path,
+			},
+			{
+				where: {
+					id,
+				},
+			},
+		);
+
+		return res.status(200).json();
 	}
 
 	async index(req, res) {
